44340ad7154f7101f80fce4aea50153ccbd902d7
Dimitar Dimitrov <dmdimitrov@infragistics.com>
2019-08-21 11:33:48 +0300

    Server configuration
    Added implementation of IRevealSdkContext, modified ConfigureServices method in Startup and created folder Dashboards in project structure.

---
+++ RevealSDK.Web.Demos/Demo1/RevealSDK/RevealSdkContext.cs
@@ -0,0 +1,29 @@
+﻿using Infragistics.Sdk;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Demo1.RevealSDK
+{
+    public class RevealSdkContext : IRevealSdkContext
+    {
+        public IRVDataSourceProvider DataSourceProvider => throw new NotImplementedException();
+
+        public IRVDataProvider DataProvider => throw new NotImplementedException();
+
+        public IRVAuthenticationProvider AuthenticationProvider => throw new NotImplementedException();
+
+        public Task<Stream> GetDashboardAsync(string dashboardId)
+        {
+            var resourceName = $"Demo1.Dashboards.{dashboardId}";
+            var assembly = Assembly.GetExecutingAssembly();
+            return Task.FromResult(assembly.GetManifestResourceStream(resourceName));
+        }
+
+        public Task SaveDashboardAsync(string userId, string dashboardId, Stream dashboardStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- RevealSDK.Web.Demos/Demo1/Startup.cs
+++ RevealSDK.Web.Demos/Demo1/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Demo1.RevealSDK;
+using Infragistics.Sdk;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -33,6 +35,13 @@ namespace Demo1
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddRevealServices(new RevealEmbedSettings
+            {
+                CachePath = @"C:\Temp"
+            }, new RevealSdkContext());
+
+            services.AddMvc().AddReveal();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
